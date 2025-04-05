49510200814f87b8b3cb667ef20411ad9f8c37cd
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-05 19:31:06 +0530

    [Sanya] Add: Create a class FileProcessor and check if content is written and read correctly

---
+++ src/main/java/com/bridgelabz/basicProblems/FileProcessing.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.basicProblems;
+
+import java.io.*;
+import java.util.*;
+
+public class FileProcessing {
+
+    public void writeToFile(String filename, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+        }
+    }
+
+    public String readFromFile(String filename) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/bridgelabz/basicProblemsTest/FileProcessingTest.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.basicProblemsTest;
+
+import com.bridgelabz.basicProblems.FileProcessing;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class FileProcessingTest {
+    private FileProcessing fp;
+    private String testFile="file.txt";
+
+    @BeforeEach
+    void setUp(){
+        fp=new FileProcessing();
+    }
+
+    @Test
+    public void shouldReadWriteFile() throws IOException {
+        String text="My name is Sanya Khanna";
+        fp.writeToFile(testFile, text);
+
+        Assertions.assertTrue(new java.io.File(testFile).exists());
+        Assertions.assertEquals(text, fp.readFromFile(testFile));
+    }
+
+    @Test
+    public void testReadNonExistentFile() {
+        Assertions.assertThrows(IOException.class, () -> fp.readFromFile("nonexistent.txt"));
+    }
+
+}
